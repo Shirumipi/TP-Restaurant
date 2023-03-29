7008174137096a31c76dff27aba91c2525120f06
Mukai Shiru <shiru@duck.com>
2023-03-29 14:02:18 +0900

    Few fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'header-img': "linear-gradient(to bottom, rgba(0,0,0,0.5), rgba(54,54,54,0.8), url('/img/eggs.jpg')",
+      },
+      colors: {
+        'dark': '#000000eb',
+        'grey': '#36363680',
+        'greyish': '#1c1c1c66',
+      },
+      spacing: {
+        '1440': '90rem',
+        '612': '38.25rem',
+      },
+    },
   },
   plugins: [],
 }
