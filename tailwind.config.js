f647319a076024fb4c86f769a9dda78687f643cb
Mukai Shiru <shiru@duck.com>
2023-03-30 07:30:22 +0900

    Picture changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'header-img': "linear-gradient(to bottom, rgba(0,0,0,0.5), rgba(54,54,54,0.8), url('/img/eggs.jpg')",
+        'header-img': "linear-gradient(to bottom, rgba(0,0,0,0.5), rgba(54,54,54,0.8), rgba(28,28,28,0.4)), url('/img/salad-chicken.jpg')",
       },
       colors: {
         'dark': '#000000eb',
