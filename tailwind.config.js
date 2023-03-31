6770c8fd153adbed211e77d9c8eacef17c8b8675
Mukai Shiru <shiru@duck.com>
2023-03-31 13:12:55 +0900

    Navbar almost finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,8 @@ module.exports = {
         'header-img': "linear-gradient(to bottom, rgba(0,0,0,1), rgba(54,54,54,0.8) 39%, rgba(28,28,28,0.4)), url('/img/salad-chicken.jpg')",
       },
       colors: {
+        'primary': '#c89446',
+        'paragraph': '#6f6f6f',
         'dark': '#000000eb',
         'grey': '#36363680',
         'greyish': '#1c1c1c66',
@@ -15,9 +17,22 @@ module.exports = {
         '1440': '90rem',
         '612': '38.25rem',
       },
+      letterSpacing: {
+        '2': '2px',
+      },
       fontFamily: {
         'pinyon': ['pinyon', 'sans-serif'],
         'source-sans-pro': ['source sans pro', 'sans-serif'],
+      },
+      margin: {
+        '81': '33px',
+      },
+      padding: {
+        '1.75': '7px',
+        '81': '33px',
+      },
+      borderWidth: {
+        '1': '1px',
       },
     },
   },
