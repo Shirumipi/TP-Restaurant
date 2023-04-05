9b37876f1e07a74f3b7081d71129bb76d568d1df
Mukai Shiru <shiru@duck.com>
2023-04-06 07:54:07 +0900

    Second card almost finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     extend: {
       backgroundImage: {
         'header-img': "linear-gradient(to bottom, rgba(0,0,0,1), rgba(54,54,54,0.8) 39%, rgba(28,28,28,0.4)), url('/img/salad-chicken.jpg')",
+        'tutorial': "linear-gradient(to bottom, rgba(28,28,28,0.6), rgba(28,28,28,0.6)), url('/img/tutorial.jpg')",
       },
       colors: {
         'primary': '#c89446',
@@ -34,6 +35,7 @@ module.exports = {
       },
       height: {
         '269': '269px',
+        '328': '328px',
         '425': '425px',
         '455': '455px',
       },
@@ -41,6 +43,7 @@ module.exports = {
         '5': '5px',
         '9': '9px',
         '20': '20px',
+        '28': '28px',
         '33': '33px',
         '38': '38px',
         '43': '43px',
@@ -50,6 +53,7 @@ module.exports = {
         '70': '70px', 
         '90': '90px',
         '94': '94px',
+        '130': '130px',
       },
       padding: {
         '1.75': '7px',
@@ -58,9 +62,12 @@ module.exports = {
         '170': '170px',
       },
       width: {
+        '219': '219px',
         '350': '350px',
         '445': '445px',
+        '540': '540px',
         '919': '919px',
+        '1013': '1013px',
       },
       borderWidth: {
         '1': '1px',
